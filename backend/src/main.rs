64015cd779cf8feac028a2f79bb7126d24632dc3
chomosuke <a13323600@gmail.com>
2021-11-10 09:46:51 +1100

    rocket hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,11 @@
-#[macro_use] extren crate rocket;
+#[macro_use] extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    
+    return "Hello, world!";
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
