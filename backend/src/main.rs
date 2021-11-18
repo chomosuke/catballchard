6da5aa3431f2777d8c0d5b601b1088a72927bae0
chomosuke <a13323600@gmail.com>
2021-11-18 17:55:17 +1100

    refactored command line args

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,7 @@
 use rocket::fs::FileServer;
 use rocket_cors::CorsOptions;
 use std::env;
+use std::collections::HashMap;
 
 #[get("/hello")]
 fn hello() -> &'static str {
@@ -10,11 +11,31 @@ fn hello() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
+
+    // initialize the server
     let mut server = rocket::build()
         .mount("/api", routes![hello])
         .mount("/", FileServer::from("../web_build"));
-    if env::args().collect::<Vec<String>>().pop().unwrap() == "debug" {
+
+    // format the commandline options
+    let in_args: Vec<String> = env::args().collect();
+    let mut args: HashMap<String, Vec<String>> = HashMap::new();
+    let mut current_key: String = String::from("default");
+    args.insert(current_key.clone(), Vec::new());
+    for in_arg in in_args {
+        if in_arg.as_bytes()[0] == '-' as u8 {
+            current_key = in_arg;
+            args.insert(current_key.clone(), Vec::new());
+        } else {
+            args.get_mut(&current_key).unwrap().push(in_arg);
+        }
+    }
+
+    // if -d, run in debug mode which allows CORS
+    if args.contains_key("-d") {
         server = server.attach(CorsOptions::default().to_cors().unwrap());
     }
+
+    // launch
     server
 }
