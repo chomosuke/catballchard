499cbe3d7b68305e38e0f92ca7187c5a7cc5563c
chomosuke <a13323600@gmail.com>
2021-11-21 04:14:49 +1100

    add /api/<id> end point

---
+++ backend/src/api/get.rs
@@ -0,0 +1,24 @@
+use mongodb::bson::{oid::ObjectId, doc};
+use rocket::{get, serde::{json::Json, Serialize}, State};
+use crate::db::DB;
+
+#[derive(Serialize)]
+pub struct Res {
+    image_url: String,
+    name: String,
+}
+
+#[get("/<id>")]
+pub async fn get(db: &State<DB>, id: &str) -> Option<Json<Res>> {
+    let _id: ObjectId;
+    if let Ok(id) = ObjectId::parse_str(id) {
+        _id = id;
+    } else {
+        return None;
+    }
+    let name = db.names.find_one(doc! { "_id": _id }, None).await.unwrap()?;
+    return Some(Json(Res {
+        image_url: name.image_url,
+        name: name.name,
+    }));
+}
--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,2 +1,3 @@
 #[path="all.rs"] mod all_; pub use all_::all;
 #[path="add.rs"] mod add_; pub use add_::add;
+#[path="get.rs"] mod get_; pub use get_::get;
