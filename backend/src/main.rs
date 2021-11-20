b477a7256a7670fe13ffd8043b4dcd3973f7835f
chomosuke <a13323600@gmail.com>
2021-11-20 11:13:00 +1100

    successfully connected to mongodb

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,8 @@
-use rocket::{fs::FileServer, Config, launch, routes};
+use rocket::{fs::FileServer, Config, routes};
 use rocket_cors::CorsOptions;
 use std::net::IpAddr;
 use structopt::StructOpt;
+use mongodb::{bson::doc, options::ClientOptions, Client};
 
 mod api;
 use api::*;
@@ -16,10 +17,13 @@ struct Args {
 
     #[structopt(short, long)]
     address: Option<IpAddr>,
+
+    #[structopt(short, long)]
+    connection_string: String,
 }
 
-#[launch]
-fn rocket() -> _ {
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
     let args = Args::from_args();
 
     // custom config, default port 80
@@ -37,6 +41,32 @@ fn rocket() -> _ {
         server = server.attach(CorsOptions::default().to_cors().unwrap());
     }
 
+    connect(args.connection_string).await
+        .expect(format!("Can't connect to MongoDB database - {}", args.connection_string));
+
     // launch
-    server
+    server.launch().await
+}
+
+async fn connect(connection_str: String) -> mongodb::error::Result<()> {
+    // Parse your connection string into an options struct
+    let mut client_options =
+        ClientOptions::parse(connection_str)
+            .await?;
+
+    // Manually set an option
+    client_options.app_name = Some("Rust Demo".to_string());
+    // Get a handle to the cluster
+    let client = Client::with_options(client_options)?;
+    // Ping the server to see if you can connect to the cluster
+    client
+        .database("admin")
+        .run_command(doc! {"ping": 1}, None)
+        .await?;
+    println!("Connected successfully.");
+    // List the names of the databases in that cluster
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+    Ok(())
 }
