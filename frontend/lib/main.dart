56d1e4af55eb127cab768e64c6fb418178974fd8
chomosuke <a13323600@gmail.com>
2021-11-22 12:06:10 +1100

    main skeleton with add button posting a default Name

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-
-import 'main_page.dart';
+import 'package:frontend/fetch.dart';
 
 void main() {
   debugPaintSizeEnabled = true;
@@ -11,14 +10,31 @@ void main() {
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  void _addDefaultName() {
+    postAdd(NewName(baseUri.resolve('favicon.ico').toString(), 'catballchard'));
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'CatBallChard',
-        theme: ThemeData(
-          primarySwatch: Colors.brown,
+      title: 'CatBallChard',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Image.network(baseUri.resolve('favicon.ico').toString()),
+          title: const Text('CatBallChard'),
+          leadingWidth: 56 / 210 * 240,
         ),
-        home: const MainPage());
+        body: const Text('hi'),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _addDefaultName,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
