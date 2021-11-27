2855cb0e93d4560dfc61ea2fc3df8b9d3013a436
chomosuke <a13323600@gmail.com>
2021-11-27 11:30:03 +1100

    delete backend

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,3 +1,4 @@
 #[path="all.rs"] mod all_; pub use all_::all;
 #[path="add.rs"] mod add_; pub use add_::add;
 #[path="get.rs"] mod get_; pub use get_::get;
+#[path="delete.rs"] mod delete_; pub use delete_::delete;
