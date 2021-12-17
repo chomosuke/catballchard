dea86f34fb98d9666b5d7a21d5f96f993a836509
chomosuke <a13323600@gmail.com>
2021-12-17 21:42:56 +1100

    simplified account patch only update Some()

--- backend/src/api/account/patch.rs
+++ backend/src/api/account/patch.rs
@@ -1,6 +1,6 @@
 use rocket::{patch, serde::{json::Json, Deserialize}, State, http::Status};
 use crate::db::{DB, User};
-use mongodb::bson::doc;
+use mongodb::bson::{doc, Document};
 
 #[derive(Deserialize)]
 pub struct Req {
@@ -19,26 +19,17 @@ pub async fn patch(req: Json<Req>, db: &State<DB>, user: User) -> Status {
     }
 
     // don't update if None, as None update will lead to null username
-    let update = if let (
-        Some(username), Some(password)
-    ) = (
-        req.username.as_ref(), req.password.as_ref()
-    ) {
-        doc! { "$set": {
-            "username": username,
-            "password": password,
-        } }
-    } else if let Some(username) = req.username.as_ref() {
-        doc! { "$set": { "username": username } }
-    } else if let Some(password) = req.password.as_ref() {
-        doc! { "$set": { "password": password } }
-    } else {
-        doc! {}
-    };
+    let mut update = Document::new();
+    if let Some(username) = req.username.as_ref() {
+        update.insert("username", username);
+    }
+    if let Some(password) = req.password.as_ref() {
+        update.insert("password", password);
+    }
+    let update = doc! { "$set": update };
 
-    db.users.update_one(doc! {
-            "_id": user._id,
-        },
+    db.users.update_one(
+        doc! { "_id": user._id },
         update,
         None,
     ).await.unwrap();
